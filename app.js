c0470a2286f661a05d3bf867e2eb50f7882b6952
givemoredmvumba-blip <givemoredmvumba@gmail.com>
2025-12-08 17:58:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,98 +1,68 @@
-// const express = require("express");
-// const app = express();
-// const port = process.env.PORT || 3001;
 
-// app.get("/", (req, res) => res.type('html').send(html));
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
-// const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+import express from "express";
+import axios from "axios";
+import * as fs from 'fs'; 
+import path from 'path';  
 
-// server.keepAliveTimeout = 120 * 1000;
-// server.headersTimeout = 120 * 1000;
-
-// const html = `
-// <!DOCTYPE html>
-// <html>
-//   <head>
-//     <title>Hello from Render!</title>
-//     <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-//     <script>
-//       setTimeout(() => {
-//         confetti({
-//           particleCount: 100,
-//           spread: 70,
-//           origin: { y: 0.6 },
-//           disableForReducedMotion: true
-//         });
-//       }, 500);
-//     </script>
-//     <style>
-//       @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-//       @font-face {
-//         font-family: "neo-sans";
-//         src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-//         font-style: normal;
-//         font-weight: 700;
-//       }
-//       html {
-//         font-family: neo-sans;
-//         font-weight: 700;
-//         font-size: calc(62rem / 16);
-//       }
-//       body {
-//         background: white;
-//       }
-//       section {
-//         border-radius: 1em;
-//         padding: 1em;
-//         position: absolute;
-//         top: 50%;
-//         left: 50%;
-//         margin-right: -50%;
-//         transform: translate(-50%, -50%);
-//       }
-//     </style>
-//   </head>
-//   <body>
-//     <section>
-//       Hello from Render!
-//     </section>
-//   </body>
-// </html>
-// `
-// Import Express.js
-const express = require('express');
-
-// Create an Express app
 const app = express();
-
-// Middleware to parse JSON bodies
 app.use(express.json());
 
-// Set port and verify_token
-const port = process.env.PORT || 3000;
-const verifyToken = process.env.VERIFY_TOKEN;
+const { WEBHOOK_VERIFY_TOKEN, GRAPH_API_TOKEN, PORT } = process.env;
 
-// Route for GET requests
-app.get('/', (req, res) => {
-  const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
+function createRandomString(length) {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
 
-  if (mode === 'subscribe' && token === verifyToken) {
-    console.log('WEBHOOK VERIFIED');
+
+app.post("/webhook", async (req, res) => {
+  // log incoming messages
+  console.log("Incoming webhook message:", JSON.stringify(req.body, null, 2));
+
+  // check if the webhook request contains a message
+  // details on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
+  const message = req.body.entry?.[0]?.changes[0]?.value?.messages?.[0];
+ 
+  res.sendStatus(200);
+});
+
+
+// accepts GET requests at the /webhook endpoint. You need this URL to setup webhook initially.
+// info on verification request payload: https://developers.facebook.com/docs/graph-api/webhooks/getting-started#verification-requests
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  // check the mode and token sent are correct
+  if (mode === "subscribe" && token === WEBHOOK_VERIFY_TOKEN) {
+    // respond with 200 OK and challenge token from the request
     res.status(200).send(challenge);
+    console.log("Webhook verified successfully!");
   } else {
-    res.status(403).end();
+    // respond with '403 Forbidden' if verify tokens do not match
+    res.sendStatus(403);
   }
 });
-
-// Route for POST requests
-app.post('/', (req, res) => {
-  const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
-  console.log(`\n\nWebhook received ${timestamp}\n`);
-  console.log(JSON.stringify(req.body, null, 2));
-  res.status(200).end();
+// async (req, res) => {
+app.get("/", (req, res) => {
+  res.send(`<pre>Nothing to see here.
+Checkout README.md to start.</pre>`);
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`\nListening on port ${port}\n`);
+
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`);
 });
