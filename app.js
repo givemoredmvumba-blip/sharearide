b958b4f01f51d40a67b274475b8f3117d767edbb
givemoredmvumba-blip <givemoredmvumba@gmail.com>
2025-12-08 18:26:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
  */
 
 import express from "express";
-// import axios from "axios";
+import axios from "axios";
 import * as fs from 'fs'; 
 import path from 'path';  
 
@@ -33,6 +33,29 @@ app.post("/webhook", async (req, res) => {
   // check if the webhook request contains a message
   // details on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
   const message = req.body.entry?.[0]?.changes[0]?.value?.messages?.[0];
+   // extract the business number to send the reply from it
+    const business_phone_number_id =
+      req.body.entry?.[0].changes?.[0].value?.metadata?.phone_number_id;
+    const username = req.body.entry?.[0]?.changes[0]?.value?.contacts?.[0];
+
+          await axios({
+                method: "POST",
+                url: `https://graph.facebook.com/v22.0/${business_phone_number_id}/messages`,
+                headers: {
+                  Authorization: `Bearer ${GRAPH_API_TOKEN}`,
+                },
+                data: {
+                  messaging_product: "whatsapp",
+                  to: message.from,
+                  text: { 
+                    body: "Hi! "+ username.profile.name + "olas"
+                  },
+                  context: {
+                    message_id: message.id, // shows the message as a reply to the original user message
+                    
+                  },
+                },
+              });
  
   res.sendStatus(200);
 });
